b19e60eea0314483b59cafe31ed6702a61ead2cb
CHIKOJgg <110553503+CHIKOJgg@users.noreply.github.com>
2023-08-20 09:45:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const modalButtons = document.querySelectorAll('.modal-button');
+const modals = document.querySelectorAll('.modal');
+const closeButton = document.querySelectorAll('.close-button');
+let swiper = new Swiper('.swiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  coverflowEffect: {
+    rotate: 0,
+    strech: 0,
+    depth: 0,
+    modifier: 3,
+    slideshadows: true,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+    1560: {
+      slidesPerView: 3,
+    },
+  },
+});
+function showScreen(screenId) {
+  const screens = document.querySelectorAll('.screen');
+  screens.forEach((screen) => screen.classList.remove('active-screen'));
+  const selectedScreen = document.getElementById(screenId);
+  selectedScreen.classList.add('active-screen');
+}
